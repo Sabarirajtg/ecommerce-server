1eeb2e6033fe67f0a0173fee88608fdb448c37d6
Saravanan K <53828581+saravanankish@users.noreply.github.com>
2022-02-27 15:26:25 +0530

    test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 mongoose.connect(
-  "mongodb+srv://newUser:merncrud@cluster0.hgmf2.mongodb.net/vijay?retryWrites=true&w=majority",
+  "mongodb+srv://admin:admin@infotix.yvteh.mongodb.net/testDB?retryWrites=true&w=majority",
   { useNewUrlParser: true }
 );
 
