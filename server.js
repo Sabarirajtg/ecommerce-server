f1ed5f0669ebc72fd710aef637e6ac3a6395952a
Sabarirajtg <71078932+Sabarirajtg@users.noreply.github.com>
2022-02-27 15:14:54 +0530

    done

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const serverless = require("serverless-http");
 const app = express();
 
 app.use(cors());
@@ -17,9 +18,16 @@ db.once("open", () => console.log("connected to database"));
 
 app.use(express.json());
 
-app.use("/", require("./routes/index"));
+app.get("/", (req, res) => {
+  res.send("heyyy");
+});
+// app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
 app.use("/products", require("./routes/Product"));
 app.use("/purchases", require("./routes/Purchase"));
 
-app.listen(process.env.PORT || 4000, () => console.log("server started at 8080"));
+app.listen(process.env.PORT || 4000, () =>
+  console.log("server started at 8080")
+);
+
+module.exports.handler = serverless(app);
